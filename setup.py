c34262ccf1f65aad78f60748ae8dcf11bc0c68b1
asofcs <67157807+asofcs@users.noreply.github.com>
2024-08-07 16:20:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-#add setup.py configuration 
+from setuptools import setup
+
+setup(
+  name='First_Steps_Python',
+  version='1.0',
+  author=@asofcs,
+  licence="MIT",
+  packages=['First_Steps_Python'], 
+  install_requires=[], # external dependencies,
+  scripts=[
+           'functions/x', 
+           'functions/y',
+          ]
+)
