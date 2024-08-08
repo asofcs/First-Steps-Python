8563c2c690634aae67fd90d025abe22ac94b8d81
asofcs <67157807+asofcs@users.noreply.github.com>
2024-08-08 15:37:14 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# add main.py configuration
+import functions as fcts
+
+def main():
+    
+    fcts.assign_namevar('test')
+    
+    globals()['test'] = fcts.assign_dim('test', 1, 1)
+    
+    globals()['test'] = fcts.assign_values('test', {0: [3], 1: [5]})
+    
+    print(globals()['test'])
+
+
+if __name__ == "__main__":
+    main()
+
