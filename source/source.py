2aa3391bfb9d326048f9136788f7866e8bd38a16
asofcs <67157807+asofcs@users.noreply.github.com>
2024-08-09 00:45:00 +0100

    Update source.py

--- source/source.py
+++ source/source.py
@@ -1,5 +1,9 @@
-def check_instance(var, xtype, verror):
-    """
+import typing as tp
+
+def check_instance(var : tp.Any , xtype : tp.Any, verror : tp.Any)->tp.Any:
+    
+    """ Check instance/type of a variable
+    
     # parameters:
     #  var: variable to check
     #  xtype: type expected
