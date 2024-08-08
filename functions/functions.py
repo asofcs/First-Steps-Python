b5981fe390c328ecfd97a2dc8be0c9e3a3fe3f76
asofcs <67157807+asofcs@users.noreply.github.com>
2024-08-08 14:45:38 +0100

    Add files via upload

--- functions/functions.py
+++ functions/functions.py
@@ -17,271 +17,83 @@ def assign_col(name : tp.Optional[str] = None,
 
     # return: IF success: TRUE. ELSE: FALSE
     """
+    
     if ((values is not None) and
+        (name is not None) and
         (len(values.keys()) >= icol) and
         (len(values[icol]) == nindex)):
 
-        globals()[
+        globals()[f'{name}'][icol] = values[icol]
+        
         return True
+        
     else:
+        
         return False
-    SDDSD
-    SDD
-
-def assign_col():
     
-    """ Fill column with index icol
+
+def assign_namevar(name: tp.Optional[str] = None)->None:
+    
+    """ Create variable (dataframe's name)
     
     # parameters:
     #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
 
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
+    # return: - . IF success: Assign name of dataframe.
+    """
     
-    """ Fill column with index icol
+    if (f'{name}' not in globals().keys()):
+        
+        globals()[f'{name}'] = None
+
+def assign_dim(name : tp.Optional[str] = None,
+               nindex : tp.Optional[int] = None,
+               ncols : tp.Optional[int] = None,)->pd.DataFrame:
+
+    """ Create dataframe with required dimensions
+
+    # parameters:
+    #  name: name of the dataframe. Type: str.
+    #  nindex: total number of indexes. Type: int.
+    #  ncols: total number of columns. Type: int.
+
+    #return: IF success: Empty dataframe with required dimensions. ELSE: None.
+    """
+
+    if (f'{name}' in globals().keys() and
+        (ncols > 0) and
+        (nindex > 0)):
+        
+        globals()[f'{name}'] = pd.DataFrame(columns = range(ncols), index = range(nindex))
+
+    return globals().[f'{name}']
+
+def assign_values(name : tp.Optional[str] = None,
+                  values : tp.Optional[dict] = None,)->pd.DataFrame:
+    
+    """ Fill dataframe by columns
     
     # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
+    #  name: name of the dataframe. Type: str.
+    #  values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.  
 
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
+    # return: IF success: Filled dataframe with ncols. ELSE: Empty dataframe or None.
+    """
+    testCol = False
+    
+    if((f'{name}' in globals().keys()) and
+      (values is not None) and
+      (len(values.keys()) > 0 ):
+        
+        for idCol in range(globals().[f'{name}'].shape[1]):
+            
+            testCol = assign_col(f'{name}', globals().[f'{name}'].shape[0], idCol, values)
 
-def assign_col():
+            if testCol is False:
+                
+                continue
+                
+    del testCol
     
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-    
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-    
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
-def assign_col():
-    
-    """ Fill column with index icol
-    
-    # parameters:
-    #   name: name of the dataframe. Type: str. 
-    #   nindex: total number of indexes. Type: int.
-    #   icol: index of the column to fill. Type: int.
-    #   values: dictionary with values to fill. Type: dict. key: column, list of values: values to fill.
-
-    # return: IF sucess: TRUE. ELSE: FALSE
-    """G
-    SDSD
-    SDDSD
-    SDD
-
+    return globals().[f'{name}']
 
