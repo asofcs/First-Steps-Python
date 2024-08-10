8ff4ee32cddf447203952c3b5737265932f6f39f
asofcs <67157807+asofcs@users.noreply.github.com>
2024-08-10 16:38:08 +0100

    Update source.py

--- source/source.py
+++ source/source.py
@@ -2,8 +2,79 @@ import sklearn.datasets
 import typing 
 import numpy
 import pandas 
+import matplotlib.pyplot
 
 
+
+def heat_map(xLegend, yLegend, sTitle, valMatrix):
+
+        #https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
+        
+        fig, ax = matplotlib.pyplot.subplots()
+        im = ax.imshow(valMatrix)
+
+        
+        # Show all ticks and label them with the respective list entries
+        ax.set_xticks(numpy.arange(len(xLegend)), labels = xLegend)
+        ax.set_yticks(numpy.arange(len(yLegend)), labels = yLegend)
+        
+        # Rotate the tick labels and set their alignment.
+        matplotlib.pyplot.setp(ax.get_xticklabels(), rotation=45, ha="right",rotation_mode="anchor")
+
+        # Loop over data dimensions and create text annotations.
+        for i in range(len(yLegend)):
+            for j in range(len(xLegend)):
+                text = ax.text(j, i, round(valMatrix[i, j], 2), ha="center", va="center", color="w")
+
+
+        ax.set_title(sTitle)
+        fig.tight_layout()
+        matplotlib.pyplot.show()
+
+    
+
+
+def create_map_dict (origList: typing.Optional[list] = None,
+                     finalList: typing.Optional[list] = None,):
+
+    dMap = None
+    
+    if((origList is not None) and (finalList is not None)):
+        
+        if (len(origList) == len(finalList)):
+
+            dMap = {elem : finalList[idVal] for idVal, elem in enumerate(sorted(origList))}
+
+    
+
+    return dMap
+
+def inv_map(origMap: typing.Optional[dict] = None,)->dict: 
+
+    invMap = dict()
+    testFlag = False 
+    
+    try:
+        
+        if((origMap is not None) and (isinstance(origMap, dict)) and (origMap.keys())):
+            
+            testFlag = True
+        else:
+            raise Exception
+
+            
+    except(Exception, ValueError, TypeError):
+        print(f'Invalid dictionary - (inv_map)')
+        
+    if (testFlag is True):
+        
+        invMap = {origMap[idKey]: idKey for idKey in origMap.keys()}
+
+    del testFlag
+
+    return invMap
+
+                
 
 def load_dataset(name: typing.Optional[str] = None,)-> numpy.ndarray:
     
@@ -71,7 +142,7 @@ def dataset_info(name: typing.Optional[str] = None,
         
 
     if testFlag is True:
-        print('\n\tData Description:\n')
+        print('\n\tDATA DESCRIPTION:\n')
 
         if ((hasattr(dtNew, 'data')) and (data is True)):
             print('The data matrix:\n', dtNew['data'])
@@ -94,11 +165,13 @@ def dataset_info(name: typing.Optional[str] = None,
     del testFlag
     del dtNew
 
-def dataset2df(name: typing.Optional[str] = None)->pandas.DataFrame:
+def dataset2df(name: typing.Optional[str] = None)->dict:
 
     testFlag = False
     dtNew = None
     dfNew = None
+    dMap = None
+    dFinal = dict()
 
     try:
         dtNew = load_dataset(name)
@@ -112,10 +185,13 @@ def dataset2df(name: typing.Optional[str] = None)->pandas.DataFrame:
 
             
     except(Exception, ValueError, TypeError):
-        print(f'Invalid dataset - {name} (dataset_info)')
+        print(f'Invalid dataset - {name} (dataset2df)')
         
 
     if testFlag is True:
+        
+        print('\n\tDATA CLEANING:\n')
+
         
         if (hasattr(dtNew, 'data') and hasattr(dtNew, 'target')):
 
@@ -131,18 +207,63 @@ def dataset2df(name: typing.Optional[str] = None)->pandas.DataFrame:
 
             dfNew  = pandas.concat([dataDf, targetDf], axis = 1)
             dfNew.dropna(how ='any', axis = 0, inplace = True)
-           
-            print('\n\tFirst values of the data:\n',dfNew.head())
 
+            dMap = create_map_dict(dfNew['target'].unique(), dtNew['target_names'])
+            dfNew.replace({"target": dMap}, inplace=True)
+
+            dFinal['df'] = dfNew
+            dFinal['map'] = dMap
+   
+            print('First values of the data:\n',dfNew.head())
             print('Unique features:\n',dfNew['target'].unique())
-
 
             del dataDf
             del targetDf
             
     del testFlag
     del dtNew
-    return dfNew  
-  
-# get description, get scatter for least and most correlated variable with target.
-# predict and get confmatrix and scores.
+    del dMap
+    return dFinal
+
+def dataset_analysis(dFinal: typing.Optional[dict] = None):
+    testFlag = False
+    invMap = dict()
+    auxDf = None
+    valMatrix = None
+
+    try:
+        
+        if (dFinal is not None) and (all(idKey in ['df','map'] for idKey in dFinal.keys())):
+            
+            testFlag = True
+        
+        else: 
+            
+            raise Exception
+            
+    except(Exception, ValueError, TypeError):
+        
+        print(f'Invalid dictionary - {name} (dataset_analysis)')
+
+    if testFlag is True:
+        
+        print('\n\tDATA ANALISYS:\n')
+
+        print('HEAT MAP:\n')
+
+        invMap = inv_map(dFinal['map'])
+        auxDf = dFinal['df'].replace({"target": invMap}, inplace = False)
+        
+        valMatrix = numpy.array(auxDf.corr().to_numpy())
+
+        heat_map(auxDf.columns.tolist(), auxDf.columns.tolist(), "Heat Map", valMatrix)
+
+        print('DATA STATISTICS:\n')
+        print(auxDf.describe())
+
+    del valMatrix
+    del testFlag
+    del invMap
+    del auxDf
+
+## add comments and parameters in heat_map
