14770e65d689d6598552ff926cfe8d1bc272e982
asofcs <67157807+asofcs@users.noreply.github.com>
2024-08-11 23:19:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-#add code
+import source.source as scr
+#import networkx
+#import pandapower
+
+
+def main():
+    # print network information
+    network_info('case39', True, True, True,
+                 True, True, True, True,
+                 True)
+    # run pf
+    network_functions('case39', 'pf', ())
+    
+    # run topology
+    network_functions('case39', 'top', ())
+  
+if __name__ == "__main__":
+    main()
