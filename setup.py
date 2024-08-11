2897abc1fda439a1a758e3875b915c2c4229354a
asofcs <67157807+asofcs@users.noreply.github.com>
2024-08-11 11:14:22 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+  name='First_Steps_Python',
+  version='1.0',
+  author=@asofcs,
+  licence="MIT",
+  packages=['First_Steps_Python'], 
+  install_requires=['pandas==2.2.2'], # external dependencies,
+  scripts=[
+           'source/source.py', 
+          ]
+)
