993c959c2c80aea7db294661c62275713eb60939
asofcs <67157807+asofcs@users.noreply.github.com>
2024-08-12 10:53:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import source.source as scr
 
 def main():
     # print network information
-    network_info('case39', True, True, True,
+    scr.network_info('case39', True, True, True,
                  True, True, True, True,
                  True)
     # run pf (See args in https://pandapower.readthedocs.io/en/latest/powerflow/ac.html)
-    network_functions('case39', 'pf', ())
+    scr.network_functions('case39', 'pf', ())
     
     # run topology - respect_switches=True, include_lines=False (See args in https://pandapower.readthedocs.io/en/latest/topology/create_graph.html)
-    network_functions('case39', 'top', ())
+    scr.network_functions('case39', 'top', ())
   
 if __name__ == "__main__":
     main()
